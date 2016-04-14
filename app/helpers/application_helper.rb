a28886dc042a228f85c7542f1af0244ff05fe5ce
Daniel Faiole <danfaiole@gmail.com>
2016-04-14 15:52:09 -0300

    Added the Helper function to put the controller name in the class of the "div body" element, making the CSS easier to separate. Also, i put the new users form in center.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def body_container_div(&block)
+    content = block_given? ? capture(&block) : ''.html_safe
+    div_class = "container #{action_name}-#{controller_name}-container"
+    content_tag(:div, content, class: div_class, id: "body_container")
+  end
 end
